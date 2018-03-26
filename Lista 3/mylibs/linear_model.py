2cce377fd85b21e41dcccab6e1fd3e6b49734a02
lucivanbatista <lucivanbatista98@gmail.com>
2018-03-26 09:53:11 -0300

    Finished Lista 3

--- Lista 3/mylibs/linear_model.py
+++ Lista 3/mylibs/linear_model.py
@@ -1,17 +1,33 @@
 from mylibs import stats as st
+import numpy as np
 
-def d1(x, y):
-    meanX = st.mean(x)
-    meanY = st.mean(y)
-    somNum = 0.0
-    somDen = 0.0
-    for i in range(len(x)):
-        somNum += (x[i] - meanX) * (y[i] - meanY)
-        somDen += (x[i] - meanX) ** 2
-    return somNum / somDen
+class SimpleLinearRegression:
+    
+    #b1
+    def coef(self, x, y):
+        meanX = st.mean(x)
+        meanY = st.mean(y)
+        somNum = 0.0
+        somDen = 0.0
+        for i in range(len(x)):
+            somNum += (x[i] - meanX) * (y[i] - meanY)
+            somDen += (x[i] - meanX) ** 2
+        return somNum / somDen
 
-def d0(x, y):
-    meanX = st.mean(x)
-    meanY = st.mean(y)
-    d0 = meanY - d1(x, y) * meanX
-    return d0[0]
+    #b0
+    def intercept(self, x, y, b1):
+        meanX = st.mean(x)
+        meanY = st.mean(y)
+        intercept_b0 = meanY - self.b1 * meanX
+        return intercept_b0[0]
+    
+    def fit(self, x, y):
+        self.b1 = self.coef(x, y)
+        self.b0 = self.intercept(x, y, self.b1)
+        return
+    
+    def predict(self, x):
+        self.y = np.zeros(x.size)
+        for i in range(len(x)):
+            self.y[i] = float(self.b0 + self.b1 * x[i])
+        return self.y
